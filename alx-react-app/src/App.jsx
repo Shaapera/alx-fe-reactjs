964e009017a1e5e4dc2f153620e94ca52b683bfe
Shaapera <shalomshaapera@gmail.com>
2025-02-15 23:47:50 +0100

    userprofi

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
+import UserProfile from './components/UserProfile'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -30,6 +31,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
       <WelcomeMessage />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
   
 
     </>
