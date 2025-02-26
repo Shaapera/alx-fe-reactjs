80a7a24b36fc9e100fa3218849f6102445b178a9
Shaapera <shalomshaapera@gmail.com>
2025-02-26 19:58:12 +0100

    task

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -43,9 +43,17 @@ function App() {
       <Services />
       <Contact />
       <Navbar />
-      <BrowserRouter />
+      <BrowserRouter>
+        <Navbar /> {/* Include the Navbar component */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
 export default App
