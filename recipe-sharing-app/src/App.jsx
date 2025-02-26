2e67bf15747df49b1e55296eed3a4466870f3f87
Shaapera <shalomshaapera@gmail.com>
2025-02-26 23:58:45 +0100

    wahala

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,16 +1,27 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import React from "react";
 import AddRecipeForm from "./components/AddRecipeForm";
 import RecipeList from "./components/RecipeList";
 import useRecipeStore from "./components/recipeStore";
+import RecipeDetails from './components/RecipeDetails';
+
 
 function App() {
   return (
-    <div>
-      <h1>Recipe Sharing App</h1>
-      <useRecipeStore />
-      <AddRecipeForm />
-      <RecipeList />
-    </div>
+
+    <Router>
+      <div>
+        <h1>Recipe Sharing App</h1>
+        <nav>
+          <Link to="/">Home</Link> | <Link to="/add">Add Recipe</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<RecipeList />} />
+          <Route path="/add" element={<AddRecipeForm />} />
+          <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
