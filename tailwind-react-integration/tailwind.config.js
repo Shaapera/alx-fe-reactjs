c6a2fb31e5e1029ca391fa1bccad59d7512ac582
Shaapera <shalomshaapera@gmail.com>
2025-03-12 22:15:37 +0100

    2wahala git push origin main
    git commit -m wahala

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js}"],
+  purge: ["./src/**/*.{html,js}"],
   darkMode: false,
   theme: {
     extend: {},
